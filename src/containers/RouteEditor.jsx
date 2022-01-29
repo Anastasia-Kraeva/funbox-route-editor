5bbf17974c98a51d9681704f4ad376223f051403
Anastasia811625 <86957944+Anastasia811625@users.noreply.github.com>
2022-01-29 12:53:12 +0300

    created d&d for PointsList.

--- src/containers/RouteEditor.jsx
+++ src/containers/RouteEditor.jsx
@@ -10,16 +10,19 @@ function RouteEditor() {
     id: '1642930415115',
     address: 'даля 1',
     coordinates: [56.31913, 43.955528],
+    order: 2,
   },
     {
       id: '2642930415116',
       address: 'геологов 1',
       coordinates: [56.248646, 43.987661],
+      order: 3,
     },
     {
       id: '1642934782504',
       address: 'родионова 1',
       coordinates: [56.320822, 44.044084],
+      order: 1,
     }])
   const [location, setLocation] = React.useState('')
 
@@ -48,6 +51,7 @@ function RouteEditor() {
           location={location}/>
         <PointsList
           points={points}
+          setPoints={setPoints}
           onClick={deletePoint}/>
       </div>
       <RouteEditorMap
--- src/presentation/PointsList.jsx
+++ src/presentation/PointsList.jsx
@@ -1,9 +1,61 @@
-function PointsList({points, onClick}) {
+import React from 'react';
+
+function PointsList({points, setPoints, onClick}) {
+  const [currentEl, setCurrentEl] = React.useState(null)
+
+  function dragStartHandler(e, el) {
+    setCurrentEl(el)
+  }
+
+  function dragOverHandler(e) {
+    e.preventDefault()
+    e.target.style.color = '#ccc'
+  }
+
+  function dragEndHandler(e) {
+    e.target.style.color = 'wheat'
+  }
+
+  function dropHandler(e, droppableIEl) {
+    e.preventDefault()
+
+    setPoints(points.map(el => {
+      if (el.id === droppableIEl.id) {
+        return {...el, order: currentEl.order}
+      }
+      if (el.id === currentEl.id) {
+        return {...el, order: droppableIEl.order}
+      }
+      return el
+    }))
+
+    e.target.style.color = 'wheat'
+  }
+
+  const sortElements = (a, b) => (a.order > b.order) ? 1 : -1
 
   return (points &&
     <ul>
-      {points.map(el => (
-        <li key={`pl-${el.id}`}>
+      {points.sort(sortElements).map(el => (
+        <li
+          key={`pl-${el.id}`}
+          draggable={true}
+          onDragStart={(e) => {
+            dragStartHandler(e, el)
+          }}
+          onDragOver={(e) => {
+            dragOverHandler(e)
+          }}
+          onDragLeave={(e) => {
+            dragEndHandler(e)
+          }}
+          onDragEnd={(e) => {
+            dragEndHandler(e)
+          }}
+          onDrop={(e) => {
+            dropHandler(e, el)
+          }}
+        >
           {el.address}
           <span
             className="material-icons"
